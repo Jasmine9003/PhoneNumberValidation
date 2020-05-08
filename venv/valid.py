bde77c040ac6adc207db008a6b8a69c509e3da00
Jasmine Dhunna <jasmine.dhunna@RWC-WS118.local>
2020-05-07 17:11:44 -0700

    changes in valid py file

--- venv/valid.py
+++ venv/valid.py
@@ -2,7 +2,7 @@ import re
 
 
 def isValid(s):
-    # Pattern = re.compile("^(\([0-9]{3}\)-|[0-9]{3})([0-9]{3}-|[0-9]{3})([0-9]{4})$")
+
     pattern = re.compile("^\({1}[1-9][0-9]{2}\){1}-{1}[0-9]{3}-{1}[0-9]{4}$|^[1-9][0-9]{9}")
 
     if pattern.match(s):
@@ -11,4 +11,3 @@ def isValid(s):
     else:
         return "Non-matching"
 
-# |^\(?[1-9][0-9]{2}\)?-?[0-9]{3}-?[0-9]{4}$
